108f4b1ec86227c0ac00f930255f512d8c55e88f
BereketKolcha <bereketcolcha@gmail.com>
2023-02-02 14:21:20 +0300

    auth

--- lib/resources/authentication_method.dart
+++ lib/resources/authentication_method.dart
@@ -1,19 +1,20 @@
 class Authenticationmethods {
-  // Future<String> signUpuser({
-  //   required String name,
-  //   required String address,
-  //   required String email,
-  //   required String password,
-  // }) async {
-  //   name.trim();
-  //   address.trim();
-  //   email.trim();
-  //   password.trim();
-  //   String output = "Something went wrong";
+  Future<String> signUpuser({
+    required String name,
+    required String address,
+    required String email,
+    required String password,
+  }) async {
+    name.trim();
+    address.trim();
+    email.trim();
+    password.trim();
+    String output = "Something went wrong";
 
-  //   if (name != "" && address != "" && email != "" && password != "") {
-  //   } else {
-  //     output = "Please fill up everything";
-  //   }
-  // }
+    if (name != "" && address != "" && email != "" && password != "") {
+    } else {
+      output = "Please fill up everything";
+    }
+    return output;
+  }
 }
