61a8e265cbb5cb08d97256fc2161015e6f3c41fd
BereketKolcha <bereketcolcha@gmail.com>
2023-02-02 15:58:03 +0300

    upadated

--- lib/resources/authentication_method.dart
+++ lib/resources/authentication_method.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Authenticationmethods {
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   Future<String> signUpuser({
     required String name,
     required String address,
@@ -12,8 +15,35 @@ class Authenticationmethods {
     String output = "Something went wrong";
 
     if (name != "" && address != "" && email != "" && password != "") {
+      try {
+        await firebaseAuth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      } on FirebaseAuthException catch (e) {
+        output = e.message.toString();
+      }
     } else {
-      output = "Please fill up everything";
+      output = "please fillup all";
+    }
+    return output;
+  }
+
+  Future<String> signInuser({
+    required String email,
+    required String password,
+  }) async {
+    email.trim();
+    password.trim();
+    String output = "Something went wrong";
+
+    if (email != "" && password != "") {
+      try {
+        await firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } on FirebaseAuthException catch (e) {
+        output = e.message.toString();
+      }
+    } else {
+      output = "please fillup all";
     }
     return output;
   }
